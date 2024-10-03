13dd9755da0064a7c8e8d806a505bb22b1bae19d
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-10-03 23:47:31 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['www.vrsiddhartha.ac.in','www.siddharthaacademy.ac.in'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: [
+      "www.vrsiddhartha.ac.in",
+      "www.siddharthaacademy.ac.in",
+      "www.google.com",
+      "videos.pexels.com",
+    ],
+  },
+};
+
+export default nextConfig;
