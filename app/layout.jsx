697d9ed45b577dfe7846097196566250939d2a76
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-10-03 23:45:44 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,38 @@
-import "./globals.css";
-import Footer from '../app/components/footer/page';
-import Header from "../app/components/header/page";
-
-export const metadata = {
-  title: "VRSEC",
-  description: "A new Millennium",
-};
+'use client';
+import { useEffect, useState } from "react";
+import StartPage from "./components/startpage/page";
+import Footer from './components/footer/page';
+import Header from './components/header/page';
 
 export default function RootLayout({ children }) {
+  const [showStartPage, setShowStartPage] = useState(false);
+
+  useEffect(() => {
+    // Check local storage to see if the user has already visited
+    const hasVisited = localStorage.getItem("hasVisited");
+    if (!hasVisited) {
+      setShowStartPage(true); // Show StartPage if not visited
+    }
+  }, []);
+
+  const handleLaunch = () => {
+    setShowStartPage(false); 
+    // Mark as visited in localStorage
+    localStorage.setItem("hasVisited", "true");
+  };
+
   return (
     <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="mB51swaTi22UaoYugKDwEZYts__M_VnMWp9cuG0BrGo" />
-      </head>
-      <body className="min-h-screen flex flex-col justify-between ">  
-        <Header />
-        <main className="" >{children}</main>
-        <Footer />
+      <body>
+        {showStartPage ? (
+          <StartPage onLaunch={handleLaunch} />
+        ) : (
+          <>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+          </>
+        )}
       </body>
     </html>
   );
