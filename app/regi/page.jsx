1c8550f3450b022d7171170a54629adf19c23c20
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-07 17:46:54 +0530

    Update page.jsx

--- app/regi/page.jsx
+++ app/regi/page.jsx
@@ -5,7 +5,7 @@ const Regi = () => {
     <div className="flex items-center justify-center ">
       <div>
         <p className="text-2xl font-bold p-4">For registration:</p>
-        <Image src="/1.png" width={300} height={300} alt="registration Qr code"/>
+        <Image src="/images/1.png" width={300} height={300} alt="registration Qr code"/>
       </div>
     </div>
   )
