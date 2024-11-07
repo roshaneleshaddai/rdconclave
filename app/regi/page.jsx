5f39c5a1b6a5aca646a6e01f65b8bb20ef56ceeb
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-07 17:51:27 +0530

    Update page.jsx

--- app/regi/page.jsx
+++ app/regi/page.jsx
@@ -5,7 +5,7 @@ const Regi = () => {
     <div className="relative font-SUSE min-h-screen w-screen mt-44 md:mt-56 flex items-center justify-center ">
      
       <div>
-        <p className="text-2xl font-bold p-4">For registration:</p>
+        <p className="text-2xl font-bold p-4">For Payment :</p>
         <Image src="/images/1.png" width={300} height={300} alt="registration Qr code"/>
       </div>
     </div>
