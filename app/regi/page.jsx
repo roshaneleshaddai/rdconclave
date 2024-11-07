cc4d1870c187b7a0eaaf01ea8e59630cbb6688f4
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-07 17:47:52 +0530

    Update page.jsx

--- app/regi/page.jsx
+++ app/regi/page.jsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import React from 'react'
 const Regi = () => {
   return (
-    <div className="flex items-center justify-center ">
+    <div className="relative font-SUSE min-h-screen w-screen mt-44 md:mt-56 flex items-center justify-center ">
+     
       <div>
         <p className="text-2xl font-bold p-4">For registration:</p>
         <Image src="/images/1.png" width={300} height={300} alt="registration Qr code"/>
