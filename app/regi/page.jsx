c53af6066631626e16ffb8f14388307a789a43cf
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-07 17:42:50 +0530

    Update page.jsx

--- app/regi/page.jsx
+++ app/regi/page.jsx
@@ -1,1 +1,14 @@
+import Image from "next/image";
+import React from 'react'
+const Regi = () => {
+  return (
+    <div className="flex items-center justify-center ">
+      <div>
+        <p className="text-2xl font-bold p-4">For registration:</p>
+        <Image src="/1.png" width={300} height={300} alt="registration Qr code"/>
+      </div>
+    </div>
+  )
+}
 
+export default Regi
