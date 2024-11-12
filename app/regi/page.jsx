b614c3f2f3576e581661f3be43d06b0936fc1bec
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-12 18:10:01 +0530

    Update page.jsx
    text size adjusted

--- app/regi/page.jsx
+++ app/regi/page.jsx
@@ -19,7 +19,7 @@ const Regi = () => {
 
   return (
     <div className="relative font-SUSE min-h-screen w-screen mt-44 md:mt-56 flex-col items-center justify-center p-4">
-        <h2 className="text-2xl font-bold mb-4 text-center">For Payment</h2>
+        <h2 className="lg:text-3xl text-2xl font-bold mb-4 text-center">For Payment</h2>
       <div className="bg-white shadow-lg rounded-lg p-6 bg-gray w-full">
         <div className="flex flex-col lg:flex-row items-center justify-evenly mb-6">
           <div><Image
