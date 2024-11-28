2b85b28db21ab41f206acfbd74ce03604ae305a2
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-28 20:13:12 +0530

    Update page.jsx
    --ng

--- app/components/header/page.jsx
+++ app/components/header/page.jsx
@@ -116,7 +116,6 @@ const Header = () => {
               width={isScrolled ? 140 : 160}
               height={isScrolled ? 140 : 160}
               className={`rounded-full object-scale-down transition-all duration-300 w-24 h-32 `}
-            loading="lazy"
             />
         </div>
         <div>
