fe2f5437773de15fae94b22b76f4833e75ea6af4
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-29 21:01:16 +0530

    Update page.jsx

--- app/contact_us/page.jsx
+++ app/contact_us/page.jsx
@@ -43,7 +43,7 @@ const contact2 =[
     <h1 className="text-2xl lg:text-4xl md:mt-60 lg:pt-10 pt-20 lg:mb-0 mb-4 font-extrabold  text-center text-black">Contact us</h1>
     <div className='flex flex-col lg:flex-row justify-evenly items-center gap-10 lg:mx-32 lg:mb-32 m-20 lg:mx-16 mx-10'>
       <div>
-         <h1 className='text-2xl lg:text-4xl font-bold pb-5 text-center flex items-center justify-center'>
+         <h1 className='text-2xl lg:text-3xl font-bold pb-5 text-center flex items-center justify-center'>
             <IoLocationOutline className="mr-3" />Our Location</h1>
          <p className='text-lg lg:text-xl text-center'>Velagapudi Ramakrishna Siddhartha Engineering College,<br />
           Chalasani Nagar,
@@ -51,7 +51,7 @@ const contact2 =[
           Vijayawada, Andhra Pradesh 520007.</p>
       </div>
       <div>
-      <h1 className='text-2xl lg:text-5xl font-bold pb-5 text-center'>Drop Us a Line</h1>
+      <h1 className='text-2xl lg:text-3xl font-bold pb-5 '>Drop us a Line</h1>
          <p className='text-lg font-bold'>Faculty Coordinators:</p>
          <div className='text-base lg:text-xl '>
           {contact1.map((c1)=>{
@@ -59,7 +59,7 @@ const contact2 =[
          ) })}
         
          </div>
-         <p className='text-lg pt-2'>Student Coordinators:</p>
+         <p className='text-lg pt-2 font-bold'>Student Coordinators:</p>
          <div className='text-base lg:text-xl '>
           {contact2.map((c1)=>{
            return( <li key={c1.id} className="flex items-center"><IoCallOutline className='mr-3' />{c1.name} - {c1.ph}</li>
