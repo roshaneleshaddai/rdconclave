68d10aec76db553031b3bc82bc91397b8b4657c6
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-11-29 20:58:21 +0530

    Update page.jsx

--- app/contact_us/page.jsx
+++ app/contact_us/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IoCallOutline } from "react-icons/io5";
+import { IoCallOutline, IoLocationOutline } from "react-icons/io5";
 const contact = () => {
   const contact1 =[
     // {
@@ -40,26 +40,27 @@ const contact2 =[
     <>
     <div className="mt-44 lg:mt-92 text-justify"></div>
 
-    <h1 className="text-2xl lg:text-5xl md:mt-60 lg:pt-10 pt-20 lg:mb-0 mb-4 font-extrabold  text-center text-black">Contact Us</h1>
+    <h1 className="text-2xl lg:text-4xl md:mt-60 lg:pt-10 pt-20 lg:mb-0 mb-4 font-extrabold  text-center text-black">Contact us</h1>
     <div className='flex flex-col lg:flex-row justify-evenly items-center gap-10 lg:mx-32 lg:mb-32 m-20 lg:mx-16 mx-10'>
       <div>
-         <h1 className='text-2xl lg:text-5xl font-bold pb-5 text-center'>Our Location</h1>
-         <p className='text-lg lg:text-2xl text-center'>Velagapudi Ramakrishna Siddhartha Engineering College,<br />
+         <h1 className='text-2xl lg:text-4xl font-bold pb-5 text-center flex items-center justify-center'>
+            <IoLocationOutline className="mr-3" />Our Location</h1>
+         <p className='text-lg lg:text-xl text-center'>Velagapudi Ramakrishna Siddhartha Engineering College,<br />
           Chalasani Nagar,
           Kanuru,<br />
           Vijayawada, Andhra Pradesh 520007.</p>
       </div>
       <div>
       <h1 className='text-2xl lg:text-5xl font-bold pb-5 text-center'>Drop Us a Line</h1>
-         <p className='text-lg'>Faculty Coordinators:</p>
-         <div className='text-base lg:text-2xl '>
+         <p className='text-lg font-bold'>Faculty Coordinators:</p>
+         <div className='text-base lg:text-xl '>
           {contact1.map((c1)=>{
            return( <li key={c1.id} className="flex items-center"><IoCallOutline className='mr-3' />{c1.name} - {c1.ph}</li>
          ) })}
         
          </div>
          <p className='text-lg pt-2'>Student Coordinators:</p>
-         <div className='text-base lg:text-2xl '>
+         <div className='text-base lg:text-xl '>
           {contact2.map((c1)=>{
            return( <li key={c1.id} className="flex items-center"><IoCallOutline className='mr-3' />{c1.name} - {c1.ph}</li>
          ) })}
