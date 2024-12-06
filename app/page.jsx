b66d76e9e95c8272627289d21b8da6fc897d4c03
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2024-12-06 10:10:09 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -196,10 +196,10 @@ function HorizontalImageScroller({ images }) {
     </div>
     {/* Date, Time, and Button Section */}
     <div className="flex-1 flex flex-col items-center justify-center font-bold">
-      <p className="text-center">Registration Deadline:<span className="text-blue-800"> November 30, 2024</span></p>
-      <p className="text-center">Preliminary Round:<span className="text-blue-800"> December 2 & 3 2024(7PM)</span></p>
-      <p className="text-center">Shortlisted teams Announcement:<span className="text-blue-800"> December 20 2024</span></p>
-      <p className="text-center">Final Round:<span className="text-blue-800"> January 24, 2025, 12 PM - January 25, 2025, 12 PM</span></p>
+      <p className="text-center">Registration Deadline:<span className="text-blue-800"> January 08, 2025</span></p>
+{/*       <p className="text-center">Preliminary Round:<span className="text-blue-800"> December 2 2025</span></p> */}
+      <p className="text-center">Shortlisted teams Announcement:<span className="text-blue-800"> January 10 2025</span></p>
+      <p className="text-center">Final Round:<span className="text-blue-800"> January 24, 2025, 11 AM - January 25, 2025, 11 AM</span></p>
       <button className="p-2 m-2 bg-white text-lg border-2 border-[#4F709C]/50 hover:border-[#4F709C] rounded-full">
         Know more..
       </button>
