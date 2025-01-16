5ea689e05ff09c467dadeb0c3cac9f9fda6be82a
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2025-01-16 18:51:13 +0530

    Create page.jsx
    page added
    --ng

---
+++ app/schedule/page.jsx
@@ -0,0 +1,22 @@
+'use client';
+import React, { useState } from "react";
+  
+const Schedule = () => {
+ 
+    return (
+      <div className="relative font-SUSE min-h-screen max-w-screen mt-44 md:mt-56">
+        <div className="pb-5">
+          <div
+            className="bg-[#604CC3]/25 bg-opacity-20 p-8 lg:mt-0 mt-52 rounded-lg mx-6 transition-opacity duration-1000 "
+          >
+            <h1 className="text-4xl  font-bold text-center text-[#604CC3]">
+              Schedule
+            </h1>
+          </div>
+          <p className="text-xl text-center m-10 font-semibold">Will be updated soon!</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Schedule;
