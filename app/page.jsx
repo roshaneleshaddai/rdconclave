f9629a04386be7444f427c21a10cb65de6574b5c
Mekala jaya nanda prabhas <153074396+prabhasg5@users.noreply.github.com>
2025-12-02 11:47:20 +0530

    Add registration fee information to page

--- app/page.jsx
+++ app/page.jsx
@@ -200,6 +200,7 @@ function HorizontalImageScroller({ images }) {
       <p className="text-center">Submission Deadline:<br/><span className="text-[#002147] text-base">December 20th, 2025</span></p>
       <p className="text-center mt-2">Acceptance Notification:<br/><span className="text-[#002147] text-base">January 5th, 2026</span></p>
       <p className="text-center mt-2">Registration Deadline:<br/><span className="text-[#002147] text-base">January 10th, 2026</span></p>
+      <p className="text-center mt-2">Registration Fee:<br/><span className="text-[#002147] text-base">₹12,000</span></p>
       <p className="text-center mt-2 text-gray-500">Springer</p>
       <button className="p-2 m-2 bg-[#002147] text-white text-lg rounded-full hover:bg-[#003366]">
         Know more..
