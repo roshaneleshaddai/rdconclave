b2c81f76e1fa799d8cf03f0a44b977c3fb25ccf3
Mekala jaya nanda prabhas <153074396+prabhasg5@users.noreply.github.com>
2025-12-30 16:45:37 +0530

    Change heading from 'Journal Publication' to 'Conference Themes'

--- app/journalpublications/page.jsx
+++ app/journalpublications/page.jsx
@@ -282,7 +282,7 @@ const DownloadSchedule = () => {
         <div
           className="bg-[#002147]/10 bg-opacity-20 p-8 lg:mt-0 mt-52 rounded-lg mx-6 transition-opacity duration-1000 "
         >
-          <h1 className="text-4xl  font-bold text-center text-[#002147]">Journal Publication</h1>
+          <h1 className="text-4xl  font-bold text-center text-[#002147]">Conference Themes</h1>
           {/* <p className="text-lg text-black p-2">
             journal Publications at our college highlights the research contributions of our esteemed academic staff, showcasing their latest papers, publications, and projects. This platform fosters collaboration with peers, industry professionals, and other institutions. Through engaging poster sessions, oral presentations, and panel discussions, our journal present their groundbreaking work, encouraging rich discussions and constructive feedback. This initiative reflects our commitment to academic excellence and paves the way for future collaborative research endeavors.
           </p> */}
