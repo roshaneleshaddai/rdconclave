5d0e4c954f762be68ab19f61776e855d189c3684
Mekala jaya nanda prabhas <153074396+prabhasg5@users.noreply.github.com>
2025-12-30 16:41:36 +0530

    Refactor header layout and navigation structure
    Refactor header component for improved layout and responsiveness. Adjust image sizes and update navigation structure.

--- app/components/header/page.jsx
+++ app/components/header/page.jsx
@@ -90,65 +90,70 @@ const Header = () => {
     >
       <source src="/images/bg.mp4" type="video/mp4" />
     </video> */}
-      <div className="flex items-center justify-between md:justify-evenly md:p-2 md:mx-10 max-w-8xl px-2">
-        {/* Hamburger Menu Icon - Mobile Only (Left Side) */}
-        <div className="md:hidden flex items-center">
-          <FaBars onClick={toggleMenu} className="text-3xl cursor-pointer text-white bg-[#1a3a5c] p-2 rounded"/>
-        </div>
-
+      <div className="flex items-center justify-evenly md:p-2 md:mx-10 max-w-8xl">
         {/* Logo Section */}
-        <div className="flex items-center justify-center md:justify-evenly space-x-0 md:space-x-4 mx-auto md:mx-0">
+        <div className="flex items-center justify-evenly space-x-0 md:space-x-4 mx-2">
               <Image
             // src="/images/vrseclogo.png"
             src="/images/sahelogo.png"
             alt="VRSEC Logo"
-            width={isScrolled ? 60 : 70}
-            height={isScrolled ? 60 : 70}
-            className={`hidden lg:block object-scale-down transition-all duration-300`}
+            width={isScrolled ? 70 : 90}
+            height={isScrolled ? 70 : 90}
+            className={`lg:block hidden object-scale-down transition-all duration-300 lg:w-120 lg:h-90`}
             loading="lazy"
           />
           <Image
             // src="/images/vrseclogo.png"
             src="/images/sahelogo.png"
             alt="VRSEC Logo"
-            width={isScrolled ? 70 : 80}
-            height={isScrolled ? 70 : 80}
-            className={`lg:hidden block object-scale-down transition-all duration-300`}
+            width={isScrolled ? 110 : 120}
+            height={isScrolled ? 110 : 120}
+            className={`lg:hidden block object-scale-down transition-all duration-300 w-24 h-32 `}
             loading="lazy"
           />
+          {/* <Image
+              // src="/images/ITlogo.jpeg"
+              src="/images/it_logoo.jpg"
+              // src="/images/it_logo.jfif"
+              alt="Information Technology"
+              width={isScrolled ? 140 : 160}
+              height={isScrolled ? 140 : 160}
+              className={`hidden lg:block  object-scale-down transition-all duration-300 w-24 h-32 `}
+            /> */}
+            
 
         </div>
 
-        <div className="flex-1 md:flex-none">
-          <div className={`md:block md:text-center transition-all duration-300 ease-in-out text-center md:text-center ${isScrolled ? 'text-xs md:text-base' : 'text-sm md:text-lg'}`}>
+        <div className="">
+          <div className={`md:block md:text-center transition-all duration-300 ease-in-out ${isScrolled ? 'text-base md:text-lg' : 'text-lg md:text-xl'}`}>
 
-            <h2 className={`font-bold text-black ${isScrolled ? 'text-2xl' : 'text-2xl md:text-4xl'} ${isScrolled ? 'hidden' : ''}`}>
+            <h2 className={`font-bold text-black text-5xl ${isScrolled ? 'hidden' : ''}`}>
             SIDDHARTHA
             </h2>
-            <div className={`border-b-2 border-[#002147] mx-auto ${isScrolled ? 'hidden' : ''}`} style={{width: '80%'}}></div>
+            <div className={`border-b-2 border-[#002147] mx-auto ${isScrolled ? 'hidden' : ''}`} style={{width: '100%'}}></div>
 
-            <h2 className={`font-bold text-black text-xs md:text-sm ${isScrolled ? 'hidden' : ''}`}>
+            <h2 className={`font-bold text-black ${isScrolled ? 'hidden' : ''}`}>
             ACADEMY OF HIGHER EDUCATION
             </h2>
-            <h2 className={`font-bold text-black text-xs md:text-sm ${isScrolled ? '' : 'hidden'}`}>
+            <h2 className={`font-bold text-black  ${isScrolled ? '' : 'hidden'}`}>
             SIDDHARTHA ACADEMY OF HIGHER EDUCATION
             </h2>
 
             <h2
-                className={`font-normal text-black text-xs md:text-sm ${
-                  isScrolled ? 'text-xs' : 'text-xs md:text-xs'
+                className={`font-normal text-black ${
+                  isScrolled ? 'lg:text-sm text-xs' : 'lg:text-xs text-base'
                 }`}
               >
                 An Institution Deemed to be <span className="font-semibold">University</span>
                 <br />
-                <span className="text-black text-xs">( Under Section 3 of UGC Act, 1956)</span>
+                <span className="text-black lg:text-xs text-xs">(Under Section 3 of UGC Act, 1956)</span>
                 <br/>
-                <span className="text-xs"><span className="text-black">Kanuru, Vijayawada - 520 007</span></span>
+                <span className="lg:text-xs text-xs"><span className="text-black">Kanuru, Vijayawada - 520 007, AP. www.vrsiddhartha.ac.in</span></span>
               </h2>
             </div>
           </div>
         <div>
-        <div className={`hidden md:flex items-center transition-all duration-300 ${isScrolled ? 'space-x-2' : 'space-x-3'}`}>
+        <div className={`hidden md:flex items-center transition-all duration-300 ${isScrolled ? 'space-x-2' : 'space-x-4'}`}>
         <div className="hidden md:block">
             {/* <Image
               src="/images/ITlogo.jpeg"
@@ -172,8 +177,8 @@ const Header = () => {
             <Image
               src="/images/s.jpeg"
               alt="springer"
-              width={isScrolled ? 70 : 80} 
-              height={isScrolled ? 70 : 80}
+              width={isScrolled ? 90 : 100} 
+              height={isScrolled ? 90 : 100}
               className="rounded-full"
             />
           </div>
@@ -181,125 +186,109 @@ const Header = () => {
           <Image
               src="/images/rclogo1.png"
               alt="Research conclave"
-              width={isScrolled ? 70 : 80} 
-              height={isScrolled ? 70 : 80}
+              width={isScrolled ? 90 : 100} 
+              height={isScrolled ? 90 : 100}
               className="rounded-full"
               
             />
           </div>
+          {/* Hamburger Menu Icon */}
+          <div className="md:hidden">
+            <FaBars onClick={toggleMenu} className="text-2xl cursor-pointer" />
+          </div>
+
+          {/* Full Logo for Desktop */}
+          {/* <div className="hidden md:block">
+            <Image
+              src="/images/vikshitbharat.png"
+              alt="Azadi ka Amrit Mahostav"
+              width={isScrolled ? 90 : 100} 
+              height={isScrolled ? 90 : 100}
+              className="rounded-full"
+            />
+          </div> */}
         </div>
+        </div>
+          <div className="md:hidden">
+          {isOpen ? (
+            <FaTimes onClick={toggleMenu} className="text-xl cursor-pointer z-50 absolute right-6 top-8" /> 
+          ) : (
+            <FaBars onClick={toggleMenu} className="text-xl cursor-pointer absolute right-6 top-8"/>
+          )}
         </div>
       </div>
       
-      {/* Mobile Sidebar Navigation (Left Side) */}
-      <div
-        className={`fixed left-0 top-0 w-72 sm:w-80 h-screen bg-[#002147] text-white transition-transform duration-300 z-40 overflow-y-auto pt-16 sm:pt-20 ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
-        }`}
+
+      {/* Mobile Navigation Menu */}
+      <nav
+        className={`fixed left-0 top-0 w-full h-full bg-[#002147] text-white text-xl flex flex-col justify-center items-center transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
       >
-        {/* Close Button in Sidebar */}
-        <div className="flex justify-end px-5 sm:px-6 py-3 sm:py-4 border-b border-[#FFD700] sticky top-0 bg-[#002147]">
-          <FaTimes 
-            onClick={toggleMenu} 
-            className="text-3xl cursor-pointer text-[#FFD700]" 
-          />
-        </div>
+        <ul className="space-y-6 text-center ">
+        <li><Link
+        href="/#home"
+        className="" onClick={toggleMenu}>
+      Home
+      </Link>
+      </li>
+      <li><Link
+      href="/#about"
+      className="" onClick={toggleMenu}>
+        About
+        </Link></li>
 
-        {/* Sidebar Navigation Links */}
-        <nav className="flex flex-col">
-          <Link
-            href="/#home"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] border-b border-gray-700 transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
+        <li><Link
+          href="/meet"
+          className="" onClick={toggleMenu}>
+          Industry-Academia Meet
+        </Link></li>
+        <li><Link
+          href="/hackathon"
+          className="" onClick={toggleMenu}>
+          inSAHEthon
+        </Link></li>
+        <li><Link
+          href="/journalpublications"
+          className="" onClick={toggleMenu}>
+          Conference Themes
+        </Link></li>
+        <li><Link
+          href="/speakers"
+          className="" onClick={toggleMenu}>
+          Speakers
+        </Link></li>
+        <li><Link
+          href="/schedule"
+          className="" onClick={toggleMenu}>
+          Schedule
+        </Link></li>
+        <li>
+          <select
+            className="text-black w-3/4 rounded p-2 bg-white shadow-md focus:ring-2 focus:ring-blue-500 text-lg"
+            defaultValue=""
+            onChange={(e) => {
+              toggleMenu();
+              downloadFile(e.target.value);
+              e.target.value = "";
+            }}
           >
-            Home
-          </Link>
-          <Link
-            href="/#about"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] border-b border-gray-700 transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
-          >
-            About
-          </Link>
-          <Link
-            href="/meet"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] border-b border-gray-700 transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
-          >
-            Industry-Academia Meet
-          </Link>
-          <Link
-            href="/hackathon"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] border-b border-gray-700 transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
-          >
-            inSAHEthon
-          </Link>
-          <Link
-            href="/journalpublications"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] border-b border-gray-700 transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
-          >
-            Journal Publication
-          </Link>
-          <Link
-            href="/speakers"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] border-b border-gray-700 transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
-          >
-            Speakers
-          </Link>
-          <Link
-            href="/schedule"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] border-b border-gray-700 transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
-          >
-            Schedule
-          </Link>
-
-          {/* Download Dropdown in Sidebar */}
-          <div className="px-5 sm:px-6 py-4 sm:py-5 border-b border-gray-700">
-            <label className="block text-xs sm:text-sm font-semibold text-[#FFD700] mb-3">
-              Download Templates
-            </label>
-            <select
-              className="w-full bg-white text-black rounded p-2.5 text-xs sm:text-sm font-semibold focus:ring-2 focus:ring-[#FFD700] focus:outline-none"
-              defaultValue=""
-              onChange={(e) => {
-                if (e.target.value !== "") {
-                  downloadFile(e.target.value);
-                }
-                e.target.value = "";
-              }}
-            >
-              <option value="" disabled>
-                Select template
+            <option value="" disabled>
+              Download
+            </option>
+            {fileNames.map((fileName, index) => (
+              <option key={index} value={index} className="text-sm">
+                {fileName}
               </option>
-              {fileNames.map((fileName, index) => (
-                <option key={index} value={index} className="text-xs sm:text-sm">
-                  {fileName}
-                </option>
-              ))}
-            </select>
-          </div>
+            ))}
+          </select>
+        </li>
 
-          <Link
-            href="/contact_us"
-            className="px-5 sm:px-6 py-4 sm:py-5 hover:bg-[#FFD700]/20 hover:text-[#FFD700] transition-all duration-200 font-semibold text-sm sm:text-base"
-            onClick={toggleMenu}
-          >
-            Contact us
-          </Link>
-        </nav>
-      </div>
-
-      {/* Overlay for Mobile Sidebar */}
-      {isOpen && (
-        <div
-          className="fixed inset-0 bg-black/50 z-30 md:hidden"
-          onClick={toggleMenu}
-        />
-      )}
+              <li><Link
+          href="/contact_us"
+          className="" onClick={toggleMenu}>
+          Contact us
+        </Link></li>
+        </ul>
+      </nav>
 
       {/* Desktop Navigation Menu */}
       <nav className="hidden md:flex items-center justify-center ">
