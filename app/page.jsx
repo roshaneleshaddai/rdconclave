40c88849b0873450c9daf0a63da45a3a51438d33
Mekala jaya nanda prabhas <153074396+prabhasg5@users.noreply.github.com>
2026-01-06 15:40:26 +0530

    Update submission deadline to January 10th, 2026

--- app/page.jsx
+++ app/page.jsx
@@ -203,8 +203,7 @@ function HorizontalImageScroller({ images }) {
       </h1>
     </div>
     <div className="flex-1 flex flex-col items-center justify-center font-bold text-sm mt-2">
-      <p className="text-center">Submission Deadline:<br/><span className="text-[#002147] text-base">January 5th, 2026</span></p>
-      <p className="text-center mt-2">Acceptance Notification:<br/><span className="text-[#002147] text-base">January 10th, 2026</span></p>
+      <p className="text-center">Submission Deadline:<br/><span className="text-[#002147] text-base">January 10th, 2026</span></p>
       <p className="text-center mt-2">Registration Fee:<br/><span className="text-[#002147] text-base">₹12,000</span></p>
       <p className="text-center mt-2 text-gray-500">Springer</p>
       <button className="p-2 m-2 bg-[#002147] text-white text-lg rounded-full hover:bg-[#003366]">
