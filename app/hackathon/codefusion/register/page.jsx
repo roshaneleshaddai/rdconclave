66bf33f5a8ebf8c2b111ae3d178fc6ff62282394
Panduranga <panduranga1797@gmail.com>
2026-01-09 17:58:09 +0530

    change

--- app/hackathon/codefusion/register/page.jsx
+++ app/hackathon/codefusion/register/page.jsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 // import { useState, useEffect } from 'react';
 // import { ArrowLeft, Users, GraduationCap, Send, CheckCircle, AlertCircle, Info, Mail, MailOpen } from 'lucide-react';
 
